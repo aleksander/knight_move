1c23509440233d392868e8c104dfe926ca2677ae
Aleksander Usov <aleksander.usov@gmail.com>
2017-02-15 19:10:45 +0300

    add isolated points (cells) check and step back

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate libc;
 
-fn print_array(arr: &[[usize; 10]; 10]) {
+fn print_array (arr: &[[usize; 10]; 10]) {
     for y in 0..10 {
         for x in 0..10 {
             print!("{:2} ", arr[x][y]);
@@ -9,13 +9,34 @@ fn print_array(arr: &[[usize; 10]; 10]) {
     }
 }
 
+fn have_isolated_points (arr: &[[usize; 10]; 10]) -> bool {
+    for y in 0..10 {
+        for x in 0..10 {
+            if arr[x][y] == 0 {
+                let mut free_neighbors = 0;
+                for i in 0..MOVES.len() {
+                    let (dx,dy) = MOVES[i];
+                    let nx = x as isize + dx;
+                    let ny = y as isize + dy;
+                    if nx >= 0 && nx <= 9 && ny >= 0 && ny <= 9 && arr[nx as usize][ny as usize] == 0 {
+                        free_neighbors += 1;
+                    }
+                }
+                if free_neighbors == 0 { return true; }
+            }
+        }
+    }
+    false
+}
+
+const MOVES: [(isize,isize); 8] = [(1,2), (2,1), (2,-1), (1,-2), (-2,-1), (-2,1), (-1,-2), (-1,2)];
+
 fn main() {
     let mut x: isize = 0;
     let mut y: isize = 0;
-    let mut c: isize = 0;
-    let mut maxc: isize = 0;
+    let mut c: usize = 0;
+    let mut maxc: usize = 0;
     let mut f: [[usize; 10]; 10] = [[0; 10]; 10];
-    let moves: [(isize,isize); 8] = [(1,2), (2,1), (2,-1), (1,-2), (-2,-1), (-2,1), (-1,-2), (-1,2)];
     let mut i = 0;
     let mut stack: [(usize,isize,isize); 101] = [(0,0,0); 101];
 
@@ -23,9 +44,9 @@ fn main() {
         //use std::time;
         //let start = time::Instant::now();
         //for _ in 0..1_000_000_000 {
-            if i < moves.len() {
+            if i < MOVES.len() {
                 //print!("{}: try move {}: ", c, i);
-                let (dx,dy) = moves[i];
+                let (dx,dy) = MOVES[i];
                 let nx = x + dx;
                 let ny = y + dy;
                 let can_move = nx >= 0 && nx <= 9 && ny >= 0 && ny <= 9 && f[nx as usize][ny as usize] == 0;
@@ -46,6 +67,23 @@ fn main() {
                     }
                     // reset move counter
                     i = 0;
+                    if have_isolated_points(&f) {
+                        //println!("{}: isolated points!", c);
+                        //print_array(&f);
+                        //unsafe { libc::getchar(); }
+                        // do step back
+                        if c == 0 {
+                            println!("searching is done");
+                            break;
+                        }
+                        c -= 1;
+                        let (ti,tx,ty) = stack[c];
+                        i = ti + 1;
+                        x = tx;
+                        y = ty;
+                        f[x as usize][y as usize] = 0;
+                        //println!("step back");
+                    }
                 } else {
                     // next move
                     i += 1;
@@ -57,12 +95,12 @@ fn main() {
                     println!("{}", maxc);
                 }
                 // do step back
-                c -= 1;
-                if c < 0 {
+                if c == 0 {
                     println!("searching is done");
                     break;
                 }
-                let (ti,tx,ty) = stack[c as usize];
+                c -= 1;
+                let (ti,tx,ty) = stack[c];
                 i = ti + 1;
                 x = tx;
                 y = ty;
